19eae2fe0b118bdd7b56b423d075677bdbec6210
Cristel <cristel.ganna@outlook.com>
2020-07-22 00:32:29 +0200

    Updates

---
+++ precache-manifest.c3727c49cf3c56050380a39b2a84ea1e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a8146e18e36568b53930cfdda82e365",
+    "url": "/pokedex-react/index.html"
+  },
+  {
+    "revision": "257720353026dda17da6",
+    "url": "/pokedex-react/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "65d983f2543629b9680d",
+    "url": "/pokedex-react/static/css/main.81e6fac0.chunk.css"
+  },
+  {
+    "revision": "257720353026dda17da6",
+    "url": "/pokedex-react/static/js/2.1139f6da.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pokedex-react/static/js/2.1139f6da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65d983f2543629b9680d",
+    "url": "/pokedex-react/static/js/main.9997590a.chunk.js"
+  },
+  {
+    "revision": "c8d812595d0c4b957826",
+    "url": "/pokedex-react/static/js/runtime-main.971f92cb.js"
+  },
+  {
+    "revision": "83cba2ad28c4fc5ce860b2a1bd8546cb",
+    "url": "/pokedex-react/static/media/spinner.83cba2ad.gif"
+  }
+]);
